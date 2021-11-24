239e44937c9fae3d74f31f81c720e9ba02e0a3de
Oriol Burgaya Verdaguer <oriolbv@Oriols-MacBook-Air.local>
2021-11-24 19:22:13 +0100

    Frontend first structure

---
+++ client/src/components/spacesComponents/Component1/index.tsx
@@ -0,0 +1,12 @@
+// components/spacesComponents/Component1/index.tsx
+
+import businessLogic from "./bl.tsx";
+
+export default function Component1() {
+  
+  const { state and functions } = businessLogic();
+
+  return {
+    // JSX
+  }
+}
