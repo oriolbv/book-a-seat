2fccddbea286c9605092758f9e1ea0fda7c38d50
oriolbv <oriolbv@gmail.com>
2021-12-07 20:32:43 +0100

    Routes structure added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 
 // importing database 
 const database = require('./database')
+// importing routes
+const spacesRouter = require('./routes/spaces-routes')
 
 // dotenv config
 require('dotenv').config();
@@ -14,6 +16,16 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // middlewares
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cors());
 app.use(bodyParser.json());
+
+// DB connection
+database.on('error', console.error.bind(console, 'MongoDB failed to connect'))
+
+app.use('/', spacesRouter)
+
+// listening to port
+app.listen(port, () => console.log(`Currently server is running at http://localhost:${port}`))
