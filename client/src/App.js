b0ca8ccc36afd2503d5d777877b69c1f2d7ac5d7
oriolbv <oriolbv@gmail.com>
2022-01-09 19:26:43 +0100

    Removed useless client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,50 @@
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  const [data, setData] = React.useState(null);
-  
-  React.useEffect(() => {
-    fetch("/hello")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+    return (
+      <div>Header</div>
+    )
 }
 
-export default App;
+const Content = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+const Total = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
